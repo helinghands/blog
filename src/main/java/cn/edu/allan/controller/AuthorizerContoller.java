e2edd32b6724a286152005b409db9408b9bd326f
allan <1825956830@com>
2019-07-28 15:38:25 +0800

    实现github第三方登录

---
+++ src/main/java/cn/edu/allan/controller/AuthorizerContoller.java
@@ -0,0 +1,44 @@
+package cn.edu.allan.controller;
+
+import cn.edu.allan.dto.AccessTokenDTO;
+import cn.edu.allan.dto.GithubUser;
+import cn.edu.allan.provider.GithubProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizerContoller {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam("code")String code,@RequestParam("state")String state,Model model){
+        logger.info("code:" + code);
+        logger.info("state:" + state);
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        model.addAttribute("user",user);
+        return "index";
+    }
+}
